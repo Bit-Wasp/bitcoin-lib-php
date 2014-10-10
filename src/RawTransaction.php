1f5e63ccd48589d7dd020419bde2faac30959446
BitWasp <thomas@bitwasp.co>
2014-10-10 19:03:51 +0100

    Revert removal of 4c. Not convinced.

--- src/RawTransaction.php
+++ src/RawTransaction.php
@@ -1174,7 +1174,7 @@ class RawTransaction
                 $generated .= self::_dec_to_bytes(strlen($sig_array[$key]) / 2, 1)
                     . $sig_array[$key];
         }
-        $generated .= self::_dec_to_bytes(strlen($script_info['script']) / 2, 1)
+        $generated .= '4c'.self::_dec_to_bytes(strlen($script_info['script']) / 2, 1)
             . $script_info['script'];
         return $generated;
     }
